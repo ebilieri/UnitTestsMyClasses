2d114619e62ae4a078ea43181a4032abecf530c3
Emerson Bilieri Claudelino <ebilieri@gmail.com>
2019-07-16 19:58:52 -0300

    Add project files.

---
+++ MyClasses/FileProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace MyClasses
+{
+    public class FileProcess
+    {
+        public bool FileExists(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentNullException("Nome do arquivo esta vazio");
+            }
+
+            return File.Exists(fileName);
+        }
+    }
+}
---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExists()
+        {
+            // A = Arrange (Inicializar Variaveis)
+            FileProcess fp = new FileProcess();
+            // A = Act     (Invocar o método para testar)
+            bool fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+            // A = Assert  (Verificar a ação)
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExists()
+        {
+            FileProcess fp = new FileProcess();
+
+            bool fromCall = fp.FileExists(@"C:Regedit.exe");
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrWhiteSpace_ThrowsArgumentNullException()
+        {
+            FileProcess fp = new FileProcess();
+
+            fp.FileExists(string.Empty);
+        }
+
+        [TestMethod]
+        public void FileNameNullOrWhiteSpace_ThrowsArgumentNullException_UsingTryCatch()
+        {
+            FileProcess fp = new FileProcess();
+
+            try
+            {
+                fp.FileExists(string.Empty);
+            }
+            catch (ArgumentException)
+            {
+                return;
+            }
+
+            Assert.Fail("Fail Expected");
+        }
+    }
+}
